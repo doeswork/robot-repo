a6fc49cdd71eef9d08c7d4013eb87f8706211cd2
Mark <mark.rosenberg@powerhrg.com>
2024-02-10 08:49:18 -0500

    MVP function

---
+++ app/components/github/url_search_component.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+module Github
+class UrlSearchComponent < ViewComponent::Base
+
+end
+end
--- test/components/repo_file_tree_component_test.rb
+++ test/components/repo_file_tree_component_test.rb
@@ -2,7 +2,7 @@
 
 require "test_helper"
 
-class RepoFileTreeComponentTest < ViewComponent::TestCase
+class GithubRepoFileComponentTest < ViewComponent::TestCase
   def test_component_renders_something_useful
     # assert_equal(
     #   %(<span>Hello, components!</span>),
